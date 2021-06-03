ab455e3085411acd1f3c64797effa9e7ef740f68
nickabadio <na354@njit.edu>
2021-06-02 21:34:30 -0400

    Set up intents and getting memeber ids and messages

--- Monkeybot.py
+++ Monkeybot.py
@@ -6,23 +6,24 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
     for guild in client.guilds:
         if guild.name == GUILD:
             break
-
     print(
         f'{client.user} is connected to the following guild:\n'
         f'{guild.name}(id: {guild.id})\n'
     )
     
+
     guild = client.get_guild(849714682804961301)
     memberlist = guild.members
     for member in memberlist:
-        print(member)
+        print(member.id)
 
 @client.event       
 async def on_message(message):
