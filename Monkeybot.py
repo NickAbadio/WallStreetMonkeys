c4a8256469cd25438edf28ee7a06c2a8cb72b07c
nickabadio <na354@njit.edu>
2021-06-13 18:06:59 -0400

    Finished json updating for level ups

--- Monkeybot.py
+++ Monkeybot.py
@@ -1,6 +1,7 @@
 import os
 import discord
 import json
+from discord import user
 from dotenv import load_dotenv
  
 load_dotenv()
@@ -11,24 +12,53 @@ intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 users = {}
 
-@client.event
-async def on_ready():
-    
+#loads initial users dictionary is json data from servers.json
+with open("servers.json") as f:
+    users = json.load(f)
+
+
+#updates the servers.json file with new data
+def UpdateJson(users):
+    with open("servers.json","w") as f:
+        json.dump(users, f, indent=2)
+        print("Updated servers.json")
+
+
+def AddXP(users, id):
+    xp = users[str(id)]["XP"]
+    needed = users[str(id)]["neededXP"]
+    level = users[str(id)]["level"]
+    users[str(id)]["XP"] = xp+1
     
 
+    if((needed-1) == 0):
+        users[str(id)]["level"] = level + 1
+        users[str(id)]["neededXP"] = 20*(level + 1)
+    else:
+        users[str(id)]["neededXP"] = needed - 1
+
+
+    #print(users)
+
+
+
+
+
+@client.event
+async def on_ready():   
     guild = client.get_guild(849714682804961301)
     memberlist = guild.members
     for member in memberlist:
-        if member not in users:
+        print(users.keys())
+        if str(member.id) not in users.keys():
             users[str(member.id)] = {
                 "level": 1,
                 "XP": 0,
                 "neededXP": 10,
             }
-    print(users)
-    with open('servers.json','w') as f:
-        json.dump(users, f, indent=2)
-
+    
+    
+    
 
 @client.event       
 async def on_message(message):
@@ -36,6 +66,10 @@ async def on_message(message):
     if message.author == client.user:
         return
     else:
+        print(message.id)
+        AddXP(users, message.author.id)
+        #print(users)
+        UpdateJson(users)
         await message.channel.send(":eagle:")
 
     
