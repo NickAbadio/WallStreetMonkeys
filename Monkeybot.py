a23dc7a095f97a0534c16ae184187b3c391d4b0e
nickabadio <na354@njit.edu>
2021-06-15 17:31:16 -0400

    set up different xp rates based on channel

--- Monkeybot.py
+++ Monkeybot.py
@@ -5,12 +5,16 @@ from discord import user
 from dotenv import load_dotenv
  
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+TOKEN = os.getenv('DISCORD_TOKEN') #gets
 GUILD = os.getenv('DISCORD_GUILD')
 
-intents = discord.Intents.all()
+intents = discord.Intents.all() #gives discord bot the ability to get any info from the server
 client = discord.Client(intents=intents)
 users = {}
+channelXPRates = {
+    "general": 1,
+    "gamers" : 5,
+}
 
 #loads initial users dictionary is json data from servers.json
 with open("servers.json") as f:
@@ -23,34 +27,37 @@ def UpdateJson(users):
         json.dump(users, f, indent=2)
         print("Updated servers.json")
 
+#adds xp to users based on certian perams
 
-def AddXP(users, id):
-    xp = users[str(id)]["XP"]
-    needed = users[str(id)]["neededXP"]
-    level = users[str(id)]["level"]
-    users[str(id)]["XP"] = xp+1
+def AddXP(users, id, XPRate):
+    xp = users[str(id)]["XP"] #gets current xp from the user
+    needed = users[str(id)]["neededXP"] #gets current needed xp till next level up
+    level = users[str(id)]["level"] #gets current users level
+    print(xp)
+    print(XPRate)
+
+    users[str(id)]["XP"] = xp+XPRate
     
 
-    if((needed-1) == 0):
+    if((needed-XPRate) <= 0): #checks if a level up is requried else just subtracts from from neededXP
         users[str(id)]["level"] = level + 1
         users[str(id)]["neededXP"] = 20*(level + 1)
     else:
-        users[str(id)]["neededXP"] = needed - 1
+        users[str(id)]["neededXP"] = needed - XPRate
 
 
-    #print(users)
+   
 
 
 
 
 
 @client.event
-async def on_ready():   
-    guild = client.get_guild(849714682804961301)
-    memberlist = guild.members
-    for member in memberlist:
-        print(users.keys())
-        if str(member.id) not in users.keys():
+async def on_ready():   #called on start of bot to make sure all users 
+    guild = client.get_guild(849714682804961301) #gets WSM discord id (CURRENTLY USING BOT TESTING CHANNEL NOT WSM)
+    memberlist = guild.members #gets a list of all WSM users (CURRENTLY USING BOT TESTING CHANNEL NOT WSM)
+    for member in memberlist: 
+        if str(member.id) not in users.keys(): #goes through all users and checks if their in the json file if not adds them at level 1
             users[str(member.id)] = {
                 "level": 1,
                 "XP": 0,
@@ -61,13 +68,13 @@ async def on_ready():
     
 
 @client.event       
-async def on_message(message):
+async def on_message(message): #on a new message in discord chat this is called
 
     print(message)
-    if message.author == client.user:
+    if message.author == client.user: #if the message comes from the bot ignore
         return
 
-    elif (message.content.startswith('$User')):
+    elif (message.content.startswith('$User')): #command to print user info 
         embed = discord.Embed(
         title = message.author,
         description = users[str(message.author.id)]["level"],
@@ -80,9 +87,8 @@ async def on_message(message):
         embed.add_field(name='XP needed', value=users[str(message.author.id)]["neededXP"], inline=True)
 
         await message.channel.send(embed=embed)
-    else:   
-        print(message.id)
-        AddXP(users, message.author.id)
+    else:   #command to add xp if the user command wasnt called and update the json
+        AddXP(users, message.author.id, channelXPRates[str(message.channel)])
         UpdateJson(users)
         await message.channel.send(":eagle:")
 
